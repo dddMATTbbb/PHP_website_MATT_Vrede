d2c37e51b730cbbbd06a8d28c0b7e24daa4f6419
dMATTb <mra060620@gmail.com>
2024-11-25 12:40:17 +0100

    spel begin

--- spel.php
+++ spel.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MemoryLane</title>
+    <link rel="stylesheet" href="spel-styles.css">
 </head>
 <body>
-    
+    <div class="header">
+        <h1>MemoryLane</h1>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </div>
 </body>
 </html>
