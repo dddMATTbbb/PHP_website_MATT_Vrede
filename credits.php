f0949174f29a3a01fd6137927e0377f42945235f
Bob1206 <112585946+Bob1206@users.noreply.github.com>
2024-12-01 22:54:01 +0100

    credits af

--- credits.php
+++ credits.php
@@ -8,7 +8,29 @@
     </head>
 
 <body>
-    <h1>Bobby houd van billen</h1>
-    <h2>Ingolo kante</h2>
-    <p>mond kopje</p>
+    <div class="shalom">
+        <h1>Shalom silva Costa</h1>
+        <h2>Trouw & Inspiratie</h2>
+        <p>\?_?/</p>
+    </div>
+    <div class="Mattias">
+        <h1>Mattias Raimond</h1>
+        <h2>Creatief & Scherp</h2>
+        <p>;-;</p>
+    </div>
+    <div class="Tutorials">
+        <h1>Codehal</h1>
+        <h2>Opfrisser Van de Week</h2>
+        <p>^_^</p>
+    </div>
+    <div class="Extra">
+        <h1>w3Schools</h1>
+        <h2>Hoop gever</h2>
+        <p>#_#</p>
+    </div>
+    <div class="School">
+        <h1>CSG De LageWaard</h1>
+        <h2>Locatie , Vijzelaan</h2>
+        <p>Havo</p>
+    </div>
 </body>
