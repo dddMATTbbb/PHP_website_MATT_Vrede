cff754fa380fb3aabb698d1aa5746b8ce6c19ecf
dMATTb <mra060620@gmail.com>
2024-12-05 20:18:38 +0100

    Update uitleg-spel-fruit.php

--- uitleg-spel-fruit.php
+++ uitleg-spel-fruit.php
@@ -10,6 +10,34 @@
     <div class="header">
         <h1>MemoryLane</h1>
     </div>
+    <div class="uitleg">
+        <h1>Fruit tellen</h1>
+        <p>In dit spel daait alles om je geheugen en oplettendheid! Hier is hoe het werkt:</p><br><br>
+        <ul>
+            <li>Start spel:</li>
+            <p>
+                Je krijgt een scherm te zien met 30 plaatjes. De plaatjes bestaan uit vijf verschillende soorten fruit:
+                Appels, Bananen, Peren, Sinaasappels en Watermeloenen.
+            </p>
+            <li>Kijk Goed:</li>
+            <p>
+                Je hebt 5 seconden om goed te kijken naar het scherm en te onthouden hoeveel van elk fruitsoort je ziet.
+            </p>
+            <li>De plaatjes verdwijnen:</li>
+            <p>
+                Na 5 seconden verdwijnen de plaatjes, en het is aan jou om te onthouden wat je hebt gezien.
+            </p>
+            <li>Invullen:</li>
+            <p>
+                Vul in hoeveel appels, bananen, peren, sinaasappels en watermeloenen je denkt dat er waren.
+            </p>
+            <li>uistlag:</li>
+            <p>
+                Het spel controleerd je antwoorden: je ziet welke fruit soorten je goed had en welke fout.<br>
+                Ook krijg je te zien wat het juiste antwoord was.
+            </p>
+        </ul>
+    </div>
     <a href="spel.php">verder</a>
 </body>
 </html>
