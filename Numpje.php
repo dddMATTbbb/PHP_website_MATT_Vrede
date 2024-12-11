b9eafd342c0989afbbc3e3c4852a75bc4ea5777b
dMATTb <mra060620@gmail.com>
2024-12-11 10:43:39 +0100

    moeilijkheidgraad numpy toegepast

--- Numpje.php
+++ Numpje.php
@@ -10,21 +10,42 @@ function generateNumbers($count = 16, $min = 1, $max = 50) {
     return $numbers;
 }
 
+$wachtTijd = 10;
+
+if ($_SESSION["numpy-diff"] == "easy")
+{
+    $wachtTijd = 15;
+}
+elseif ($_SESSION["numpy-diff"] == "medium")
+{
+    $wachtTijd = 10;
+}
+elseif ($_SESSION["numpy-diff"] == "hard")
+{
+    $wachtTijd = 5;
+}
+else
+{
+    $wachtTijd = 10;
+}
+
+print_r($wachtTijd);
+
 // Genereer nummers en sla deze op in de sessie
 $numbers = generateNumbers();
 $_SESSION['numbers'] = $numbers;
 $_SESSION['sum'] = array_sum($numbers);
 
 // Automatisch doorverwijzen naar check.php na 10 seconden
-header("refresh:10;url=check.php");
+header("refresh:$wachtTijd;url=check.php");
 ?>
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4x4 Raster</title>
-    <link rel="stylesheet" href="style.css">
+    <title>MermoryLane</title>
+    <link rel="stylesheet" href="Numpje-style.css">
 </head>
 <body>
     <h1>Onthoud de nummers!</h1>
@@ -34,7 +55,7 @@ header("refresh:10;url=check.php");
         <?php endforeach; ?>
     </div>
     <div class="message">
-        Na 10 seconden word je doorgestuurd om het antwoord te geven.
+        <?php echo 'Na '.$wachtTijd. ' seconden word je doorgestuurd om het antwoord te geven.'?>
     </div>
 </body>
 </html>
